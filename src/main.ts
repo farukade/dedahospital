a34139b88b8b16613ff6ae4bc5e45d236fc9d71a
unknown <farukadekunle@gmail.com>
2022-03-12 13:12:37 +0100

    next-of-kin update endpoint created

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  const config = new DocumentBuilder()
-  .setTitle('Deda Hospital API')
-  .setDescription('Deda Users API')
-  .setVersion('1.0')
-  .build();
+  // const config = new DocumentBuilder()
+  // .setTitle('Deda Hospital API')
+  // .setDescription('Deda Users API')
+  // .setVersion('1.0')
+  // .build();
 
-  const document =  SwaggerModule.createDocument(app, config);
+  // const document =  SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('/', app, document)
+  // SwaggerModule.setup('/', app, document);
 
   await app.listen(3000);
 }
